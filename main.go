db4f45954db0ffff757b27aeb78c57ea3d288fad
Qadriddin <qadrdin81@gmail.com>
2023-12-11 14:21:16 +0500

    add: added business logic

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func addClient() {
 func showAllClients() {
 	fmt.Println("Список клиентов:")
 	for name, client := range clients {
-		fmt.Println(name, "- возраст:", client["age"], "- баланс:", client["balance"] c)
+		fmt.Println(name, "- возраст:", client["age"], "- баланс:", client["balance"], "c")
 	}
 	fmt.Println("______________")
 }
